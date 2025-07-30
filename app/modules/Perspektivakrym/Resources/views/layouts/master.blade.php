ed67cc51efb10e72f73f75cb70dac9340fb935d9
Semenov <m.semenov@automacon.ru>
2025-07-30 12:20:43 +0300

    Add interactive JavaScript handlers for payment actions in master.blade.php
    - Implemented jQuery event handlers for buttons to manage payment actions such as recalculating, adding planned payments, freezing, unfreezing, and generating PDFs.
    - Enhanced user experience by integrating modal popups for confirmation and actions related to payment management.

--- app/modules/Perspektivakrym/Resources/views/layouts/master.blade.php
+++ app/modules/Perspektivakrym/Resources/views/layouts/master.blade.php
@@ -26,8 +26,72 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"></script>
 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
-        {{-- Локальные JS файлы отсутствуют, закомментируем --}}
+        {{-- Локальные JS файлы --}}
         {{-- <script src="{{ asset('js/datepicker_ru.js') }}"></script> --}}
         {{-- <script src="{{ asset('js/perspektivakrym/app.js?v=1.3.2') }}"></script> --}}
+        
+        <script>
+        $(document).ready(function() {
+            // Обработчик для кнопки "Пересчитать"
+            $('.recalculate').on('click', function() {
+                var graphNumber = $(this).data('graph');
+                var dealId = $('input[name="deal_id"]').val();
+                var auth = $('input[name="auth"]').val();
+                
+                // Показываем модальное окно подтверждения
+                $('#recalculate').modal('show');
+            });
+            
+            // Обработчик для кнопки "Добавить плановый платеж"
+            $('.add-plan-pay').on('click', function() {
+                var graphNumber = $(this).data('graph');
+                var dealId = $('input[name="deal_id"]').val();
+                var auth = $('input[name="auth"]').val();
+                
+                // Показываем модальное окно добавления платежа
+                $('#add-plan-pay').modal('show');
+            });
+            
+            // Обработчик для кнопки "Заморозить все"
+            $('.freeze-all').on('click', function() {
+                var graphNumber = $(this).data('graph');
+                var dealId = $('input[name="deal_id"]').val();
+                var auth = $('input[name="auth"]').val();
+                
+                // Показываем модальное окно заморозки
+                $('#block-plan-pay').modal('show');
+            });
+            
+            // Обработчик для кнопки "Разморозить все"
+            $('.unfreeze-all').on('click', function() {
+                var graphNumber = $(this).data('graph');
+                var dealId = $('input[name="deal_id"]').val();
+                var auth = $('input[name="auth"]').val();
+                
+                // Показываем модальное окно разморозки
+                $('#unblock-plan-pay').modal('show');
+            });
+            
+            // Обработчик для кнопки "Основной график"
+            $('.get-pdf-land').on('click', function() {
+                var graphNumber = $(this).data('graph');
+                var dealId = $('input[name="deal_id"]').val();
+                var auth = $('input[name="auth"]').val();
+                
+                // Показываем модальное окно для PDF
+                $('#get-pdf-land').modal('show');
+            });
+            
+            // Обработчик для кнопки "График по подряду"
+            $('.get-pdf-contract').on('click', function() {
+                var graphNumber = $(this).data('graph');
+                var dealId = $('input[name="deal_id"]').val();
+                var auth = $('input[name="auth"]').val();
+                
+                // Показываем модальное окно для PDF
+                $('#get-pdf-contract').modal('show');
+            });
+        });
+        </script>
     </body>
 </html>
